676f8da38e69887e8bd46c3ac7a27f5cbc2a17e4
Yuri Andriaccio <yurand2000@gmail.com>
2026-01-29 19:17:58 +0100

    Initial impl

---
+++ src/lib.rs
@@ -0,0 +1,147 @@
+use std::collections::HashMap;
+
+#[macro_use]
+extern crate log;
+
+mod filesystem;
+
+pub mod prelude {
+    pub use super::{
+        HCBSController,
+    };
+}
+
+#[derive(Debug)]
+pub struct HCBSController {
+    sysinfo: sysinfo::System,
+    mountpoint: &'static str,
+    active_procs: HashMap<sysinfo::Pid, ProcessStats>,
+    last_update: std::time::Instant,
+}
+
+#[derive(Debug, Clone)]
+pub struct ProcessStats {
+    uid: sysinfo::Uid,
+    gid: sysinfo::Gid,
+    crtime: std::time::SystemTime,
+}
+
+impl HCBSController {
+    pub fn new() -> Self {
+        let mut ctrl = Self {
+            sysinfo: sysinfo::System::new(),
+            mountpoint: "/hcbs-manager",
+            active_procs: HashMap::with_capacity(0),
+            last_update: std::time::Instant::now(),
+        };
+
+        ctrl.update_active_processes();
+        ctrl
+    }
+
+    pub fn mount(self) -> anyhow::Result<()> {
+        let mountpoint = self.mountpoint;
+
+        ctrlc::set_handler(move || {
+            std::process::Command::new("umount")
+                .arg(mountpoint)
+                .output()
+                .unwrap();
+        })?;
+
+        std::fs::create_dir(mountpoint)?;
+
+        fuser::mount2(
+            self,
+            mountpoint,
+            &[
+                fuser::MountOption::AllowOther,
+                fuser::MountOption::AutoUnmount,
+                fuser::MountOption::DefaultPermissions,
+                fuser::MountOption::NoDev,
+                fuser::MountOption::NoSuid,
+                fuser::MountOption::RW,
+                fuser::MountOption::NoExec,
+                fuser::MountOption::Sync,
+            ]
+        )?;
+
+        std::fs::remove_dir(mountpoint)?;
+
+        Ok(())
+    }
+
+    fn update_active_processes(&mut self) {
+        use sysinfo::*;
+
+        const UPDATE_DELTA: std::time::Duration = std::time::Duration::from_secs(1);
+
+        let now = std::time::Instant::now();
+        if now - self.last_update <= UPDATE_DELTA {
+            return;
+        }
+
+        self.sysinfo.refresh_processes_specifics(
+            ProcessesToUpdate::All,
+            true,
+            ProcessRefreshKind::nothing()
+                .with_user(UpdateKind::Always));
+
+        self.active_procs =
+            self.sysinfo.processes().iter()
+                .filter(|(_, p)| p.exists())
+                .map(|(_, p)|
+                (p.pid(), ProcessStats {
+                    uid: p.user_id().unwrap().clone(),
+                    gid: p.group_id().unwrap().clone(),
+                    crtime: std::time::UNIX_EPOCH + std::time::Duration::from_secs(p.start_time()),
+                }))
+                .collect();
+
+        self.last_update = std::time::Instant::now();
+    }
+
+    // fn get_active_processes(&mut self) -> &[ProcessData] {
+    //     const UPDATE_DELTA: std::time::Duration = std::time::Duration::from_millis(1);
+
+    //     let now = std::time::Instant::now();
+    //     if now - self.last_update > UPDATE_DELTA {
+    //         self.update_active_processes();
+    //     }
+
+    //     &self.active_procs
+    // }
+}
+
+// fn get_processes(system: &mut sysinfo::System) -> (Vec<ProcessData>, Vec<sysinfo::Pid>) {
+//     use sysinfo::*;
+
+//     // Get all processes updates
+//     system.refresh_processes_specifics(
+//         ProcessesToUpdate::All,
+//         false,
+//         ProcessRefreshKind::nothing()
+//             .with_user(UpdateKind::Always));
+
+//     let (alive, dead): (Vec<_>, Vec<_>) =
+//         system.processes().iter()
+//         .map(|(_, p)| p)
+//         .partition(|p| p.exists());
+
+//     let alive = alive.into_iter()
+//         .map(|p| {
+//             ProcessData {
+//                 pid: p.pid(),
+//                 uid: p.user_id().unwrap().clone(),
+//                 gid: p.group_id().unwrap().clone(),
+//             }
+//         })
+//         .collect();
+
+//     let dead: Vec<_> = dead.iter().map(|p| p.pid()).collect();
+
+//     // Remove dead processes
+//     system.refresh_processes(ProcessesToUpdate::Some(&dead), true);
+
+//     (alive, dead)
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use hcbs_manager::prelude::*;
+
+#[macro_use]
+extern crate log;
+
+#[derive(Debug, Clone)]
+pub struct ProcessData {
+    pid: sysinfo::Pid,
+    uid: sysinfo::Uid,
+    gid: sysinfo::Gid,
+}
+
+fn main() -> anyhow::Result<()> {
+    // Debug Logging
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Debug)
+        .init();
+
+    HCBSController::new().mount()?;
+
+    Ok(())
 }
